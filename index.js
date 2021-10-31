e40b92b9106aba86add5f79136d8436d8a0d9992
crisboxing11 <crisboxing11@gmail.com>
2021-10-31 15:53:27 -0700

    added functionality

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-alert("This is a thing")
+const panel = document.querySelectorAll('.panel');
+
+panel.forEach((panel)=> {
+    panel.addEventListener('click', () => {
+        removeActiveClassList()
+        panel.classList.add('active')
+    })
+})
+
+const removeActiveClassList = () => {
+    panel.forEach((panel)=> {
+        panel.classList.remove('active')
+    })
+}
